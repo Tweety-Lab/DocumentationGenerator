9a5a9acd91aa90eee9bd36d5b38cb117b76a7d75
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-20 17:23:38 +1000

    Initial commit

---
+++ JSON/NavBar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentationGenerator.JSON
+{
+    public class NavBar
+    {
+        //               Title            Page Title -  Page Path
+        public List<NavTitle> Titles { get; set; } = new();
+    }
+
+    public struct NavTitle
+    {
+        public Dictionary<string, string> Pages { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// Entry Point
+
+using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DocumentationGenerator;
+using DocumentationGenerator.JSON;
+
+namespace DocumentationGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<NavTitle> navTitles = new List<NavTitle>()
+            {
+                new NavTitle()
+                {
+                    Pages = new Dictionary<string, string>()
+                    {
+                        {"test", "test_path" }
+                    }
+                }
+            };
+
+            NavBar navbar = new NavBar()
+            {
+                Titles = navTitles
+            };
+
+
+            // Write json to file
+            File.WriteAllText(".json.json", JsonSerializer.Serialize(navbar));
+        }
+    }
+}
