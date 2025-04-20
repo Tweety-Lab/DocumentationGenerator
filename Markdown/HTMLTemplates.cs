37f929aadb4dba2c1524b51ea4c8b884aa996e71
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-20 19:20:48 +1000

    Add HTML Template support.

---
+++ Markdown/HTMLTemplates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentationGenerator.Markdown
+{
+    public static class HTMLTemplates
+    {
+        public static string PageTemplate = File.ReadAllText("HTMLTemplates/test_page.html");
+    }
+}
--- Serializing/Page.cs
+++ Serializing/Page.cs
@@ -22,8 +22,14 @@ namespace DocumentationGenerator.Serializing
             {
                 Directory.CreateDirectory(directory);
             }
+            
+            // Convert Markdown to HTML
+            string HTML = MarkdownUtil.ConvertMarkdownToHtml(MarkdownContents);
 
-            File.WriteAllText(path, MarkdownUtil.ConvertMarkdownToHtml(MarkdownContents));
+            // Load the HTML into the page template
+            HTML = HTMLTemplates.PageTemplate.Replace("{{HTML_DOCUMENTATION}}", HTML);
+
+            File.WriteAllText(path, HTML);
         }
     }
 }
