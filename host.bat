dac6c8a97d2c087177cc38d8755098dc27964435
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-21 20:21:51 +1000

    Add "host.bat" automated hosting script.

--- host.bat
+++ host.bat
@@ -1,3 +1,3 @@
 dotnet build DocumentationGenerator.sln
 cd "TestDocs"
-"./DocumentationGenerator/bin/Debug/net9.0/DocumentationGenerator.exe" "Host 9999"
+"../DocumentationGenerator/bin/Debug/net9.0/DocumentationGenerator.exe" host 9999
