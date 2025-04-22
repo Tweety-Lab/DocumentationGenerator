2a59e9b6e6d86b4850b46f2658d279461c66b4a6
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-22 22:09:09 +1000

    Remove unused variable.

--- DocumentationGenerator/Utilities/CommandLineOptions.cs
+++ DocumentationGenerator/Utilities/CommandLineOptions.cs
@@ -5,5 +5,4 @@ public class CommandLineOptions
     public string? JsonFilePath { get; set; }
     public string OutputPath { get; set; } = "Build";
     public int? HostPort { get; set; }
-    public string? ThemePath { get; set; }
 }
