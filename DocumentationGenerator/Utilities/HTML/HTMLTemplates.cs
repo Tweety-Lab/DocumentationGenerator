b33fb44991038c889d2a0664d52b1f2016d9401e
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-22 17:26:34 +1000

    Move HTMLTemplates to "HTMLTemplates/Default".

--- DocumentationGenerator/Utilities/HTML/HTMLTemplates.cs
+++ DocumentationGenerator/Utilities/HTML/HTMLTemplates.cs
@@ -2,7 +2,7 @@
 
 public static class HTMLTemplates
 {
-    public static string TemplatePath { get; set; } = Path.Combine(AppContext.BaseDirectory, "../../../HTMLTemplates");
+    public static string TemplatePath { get; set; } = Path.Combine(AppContext.BaseDirectory, "../../../HTMLTemplates/Default");
     public static string PageTemplate { get; set; } = File.ReadAllText(Path.Combine(TemplatePath, "test_page.html"));
 
     public static string NavBarTemplate { get; set; } =
