99ee2607069a114131bfb5c1e083ee592b5cefeb
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-04-22 18:36:26 +1000

    Expand Minimal Theme.

--- DocumentationGenerator/Utilities/HTML/HTMLTemplates.cs
+++ DocumentationGenerator/Utilities/HTML/HTMLTemplates.cs
@@ -5,7 +5,7 @@ namespace DocumentationGenerator.Utilities.HTML;
 
 public static class HTMLTemplates
 {
-    public static Theme Theme { get; set; } = ThemeUtil.LoadTheme(Path.Combine(AppContext.BaseDirectory, "../../../HTMLTemplates/Default/theme.json"));
+    public static Theme Theme { get; set; } = ThemeUtil.LoadTheme(Path.Combine(AppContext.BaseDirectory, "../../../HTMLTemplates/Minimal/theme.json"));
     public static string PageTemplate { get; set; } = File.ReadAllText(Path.Combine(Theme.Paths.Root, Theme.Paths.Page));
     public static string NavBarTemplate { get; set; } = File.ReadAllText(Path.Combine(Theme.Paths.Root, Theme.Paths.NavBar));
 }
